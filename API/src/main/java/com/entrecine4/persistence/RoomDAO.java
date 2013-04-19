4bf0f9f3751b84cfd629da7ee8fb574bf2b4a6e2
Adolfo Fernandez Suarez <afsuarez7@gmail.com>
2013-04-19 22:54:58 +0200

    Implementado DAO y DTO de la clase Room. Falta revisión de TODO's

--- API/src/main/java/com/entrecine4/persistence/RoomDAO.java
+++ API/src/main/java/com/entrecine4/persistence/RoomDAO.java
@@ -1,5 +1,16 @@
 package com.entrecine4.persistence;
 
-public interface RoomDAO {
+import java.sql.SQLException;
+import java.util.List;
 
+import com.entrecine4.model.Room;
+
+//TODO:No javadoc yet
+public interface RoomDAO 
+{
+	public Room get(long roomId) throws SQLException;
+	public List<Room> getAll() throws SQLException;
+	public void save(Room room) throws SQLException;
+	public void update(Room room) throws SQLException;
+	public void delete(Room room) throws SQLException;
 }
