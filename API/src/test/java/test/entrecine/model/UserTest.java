cd68918e9e58e226e499219851bfc45f2124b9fd
Herminio García <herminiogg@hotmail.com>
2013-04-19 16:46:23 +0200

    Añadido al modelo la clase User y su test

---
+++ API/src/test/java/test/entrecine/model/UserTest.java
@@ -0,0 +1,47 @@
+package test.entrecine.model;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import com.entrecine4.model.User;
+
+public class UserTest {
+	
+	private User user1;
+	private User user2;
+	
+	private final static long ID = 1;
+	private final static String USERNAME = "pepito59";
+	private final static String PASSWORD = "mystrongpass";
+
+	/**
+	 * Test without parameters, this will test setters
+	 */
+	@Test
+	public void testWithoutParameters() {
+		user1 = new User();
+		user1.setId(ID);
+		user1.setUsername(USERNAME);
+		user1.setPassword(PASSWORD);
+		testAttributes(user1);
+	}
+	
+	/**
+	 * Test with parameters, this will test getters and parameterized constructor
+	 */
+	@Test
+	public void testWithParameters() {
+		user2 = new User(ID, USERNAME, PASSWORD);
+		testAttributes(user2);
+	}
+
+	/**
+	 * Method for test the attributes
+	 * @param the user to test
+	 */
+	private void testAttributes(User user) {
+		assertEquals(user.getId(), ID);
+		assertEquals(user.getUsername(), USERNAME);
+		assertEquals(user.getPassword(), PASSWORD);
+	}
+
+}
