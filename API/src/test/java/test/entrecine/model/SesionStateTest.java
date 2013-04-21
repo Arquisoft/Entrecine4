6582d2ad0a2e0b6bf4936261f04e020631587a4f
Herminio García <herminiogg@hotmail.com>
2013-04-21 12:42:38 +0200

    Añadida clase del modelo SesionState y su correspondiente test

---
+++ API/src/test/java/test/entrecine/model/SesionStateTest.java
@@ -0,0 +1,60 @@
+package test.entrecine.model;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Date;
+import org.junit.Test;
+import com.entrecine4.model.SesionState;
+
+
+/**
+ * @author Arquisoft - Entrecine4
+ *
+ */
+public class SesionStateTest {
+
+	private SesionState sessionState1;
+	private SesionState sessionState2;
+	
+	private final static long ROOM_ID = 12;
+	private final static int ROW = 3;
+	private final static int COLUMN = 8;
+	private final static Date DATE = new Date();
+	private final static long SESSION = 4;
+	
+
+	/**
+	 * Test without parameters, this will test setters
+	 */
+	@Test
+	public void testWithoutParameters() {
+		sessionState1 = new SesionState();
+		sessionState1.setRoomId(ROOM_ID);
+		sessionState1.setRow(ROW);
+		sessionState1.setColumn(COLUMN);
+		sessionState1.setDate(DATE);
+		sessionState1.setSession(SESSION);
+		testAttributes(sessionState1);
+	}
+	
+	/**
+	 * Test with parameters, this will test getters and parameterized constructor
+	 */
+	@Test
+	public void testWithParameters() {
+		sessionState2 = new SesionState(ROOM_ID, ROW, COLUMN, DATE, SESSION);
+		testAttributes(sessionState2);
+	}
+
+	/**
+	 * Method for test the attributes
+	 * @param the user to test
+	 */
+	private void testAttributes(SesionState sessionState) {
+		assertEquals(sessionState.getRoomId(), ROOM_ID);
+		assertEquals(sessionState.getRow(), ROW);
+		assertEquals(sessionState.getColumn(), COLUMN);
+		assertEquals(sessionState.getDate(), DATE);
+		assertEquals(sessionState.getSession(), SESSION);
+	}
+
+}
