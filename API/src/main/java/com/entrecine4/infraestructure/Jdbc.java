46c969b80ee9066435ff473f82c319b46c57f2f3
Herminio García <herminiogg@hotmail.com>
2013-04-22 21:02:25 +0200

    Añadida la libreria c3p0 para pool de conexiones, ahora ya se puede usar desde la clase Jdbc

--- API/src/main/java/com/entrecine4/infraestructure/Jdbc.java
+++ API/src/main/java/com/entrecine4/infraestructure/Jdbc.java
@@ -5,48 +5,55 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.DataSources;
+
 public class Jdbc {
 
-	/*TODO:Datasource
+	private static DataSource pooled;
 
 	public static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(URL, USER, PASS);
-	}*/
+		if (pooled == null) {
+			DataSource unpooled = DataSources.unpooledDataSource(
+					"jdbc:mysql://ip/as", "user", "pass");
+			pooled = DataSources.pooledDataSource(unpooled);
+		}
+		return pooled.getConnection();
+	}
 
-	public static void close(ResultSet rs, Statement st, Connection c)
-	{
+	public static void close(ResultSet rs, Statement st, Connection c) {
 		close(rs);
 		close(st);
 		close(c);
 	}
 
-	public static void close(ResultSet rs, Statement st)
-	{
+	public static void close(ResultSet rs, Statement st) {
 		close(rs);
 		close(st);
 	}
 
-	protected static void close(ResultSet rs)
-	{
+	protected static void close(ResultSet rs) {
 		if (rs != null)
 			try {
 				rs.close();
-			} catch (SQLException e) {}
+			} catch (SQLException e) {
+			}
 	}
 
-	public static void close(Statement st)
-	{
+	public static void close(Statement st) {
 		if (st != null)
 			try {
 				st.close();
-			} catch (SQLException e) {}
+			} catch (SQLException e) {
+			}
 	}
 
-	public static void close(Connection c)
-	{
+	public static void close(Connection c) {
 		if (c != null)
 			try {
 				c.close();
-			} catch (SQLException e){}
+			} catch (SQLException e) {
+			}
 	}
 }
