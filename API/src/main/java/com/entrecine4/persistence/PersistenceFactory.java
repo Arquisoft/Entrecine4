ac14008b444665076f4d8e0c0e48df46d3b26c92
Adolfo Fernández Suárez <afsuarez7@gmail.com>
2013-04-24 10:54:30 +0200

    Implementada la factoría de persistencia. Falta su Javadoc

--- API/src/main/java/com/entrecine4/persistence/PersistenceFactory.java
+++ API/src/main/java/com/entrecine4/persistence/PersistenceFactory.java
@@ -1,5 +1,14 @@
 package com.entrecine4.persistence;
 
-public interface PersistenceFactory {
-
+//TODO: No javadoc yet
+public interface PersistenceFactory 
+{
+	public EmployeeDAO createEmployeeDAO();
+	public IncidenceDAO createIncidenceDAO();
+	public MovieDAO createMovieDAO();
+	public PurchaseDAO createPurchaseDAO();
+	public RoomDAO createRoomDAO();
+	public SessionDAO createSessionDAO();
+	public SessionStateDAO createSessionStateDAO();
+	public UserDAO createUserDAO();
 }
