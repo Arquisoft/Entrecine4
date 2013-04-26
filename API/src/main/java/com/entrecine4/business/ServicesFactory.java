2f4162fc3373756f824ed1be3dcb565225ca5fa1
danielloureda <danilouloureda@gmail.com>
2013-04-26 20:33:20 +0200

    Hacer ServicesFactory e implementacion

--- API/src/main/java/com/entrecine4/business/ServicesFactory.java
+++ API/src/main/java/com/entrecine4/business/ServicesFactory.java
@@ -1,5 +1,36 @@
 package com.entrecine4.business;
 
+import com.entrecine4.business.IncidenceService;
+import com.entrecine4.business.MoviesService;
+import com.entrecine4.business.ReservationService;
+import com.entrecine4.business.RoomService;
+import com.entrecine4.business.StaffService;
+
 public interface ServicesFactory {
 
+	/**
+	 * Method to get an instance of IncidenceService and get access to the business layer
+	 * @return An instance of IncidenceService
+	 */
+	public IncidenceService createIncidenceService();
+	/**
+	 * Method to get an instance of MoviesService and get access to the business layer
+	 * @return An instance of MoviesService
+	 */
+	public MoviesService createMoviesService();
+	/**
+	 * Method to get an instance of ReservationService and get access to the business layer
+	 * @return An instance of ReservationService
+	 */
+	public ReservationService createReservationService();
+	/**
+	 * Method to get an instance of RoomService and get access to the business layer
+	 * @return An instance of RoomService
+	 */
+	public RoomService createRoomService();
+	/**
+	 * Method to get an instance of StaffService and get access to the business layer
+	 * @return An instance of StaffService
+	 */
+	public StaffService createStaffService();
 }
