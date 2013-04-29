d32365727e147076492c8ff938dd0cdb767b91ec
Herminio García <herminiogg@hotmail.com>
2013-04-29 18:15:25 +0200

    Añadidas tres pantallas en play

--- WebApp/app/controllers/Application.java
+++ WebApp/app/controllers/Application.java
@@ -8,7 +8,15 @@ import views.html.*;
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());
     }
-  
+
+    public static Result registro() {
+        return ok(registro.render());
+    }
+
+    public static Result pelicula(Long id) {
+        return ok(pelicula.render(id));
+    }
+
 }
